9fe2e2c2acb622680926d4500e16e03a0eea012c
KnollElias <knoll.elias@icloud.com>
2025-05-12 15:32:21 +0200

    Create endings.sh

---
+++ hangman/endings.sh
@@ -0,0 +1,18 @@
+mistakes0(){
+    echo "Super! 👍Du hast das Wort erraten"
+}
+mistakes1(){
+    echo "Du kommst deinem Ziel immer näher"
+}
+mistakes2(){
+    echo "Der Erfolg kommt, nicht aufgeben"
+}
+mistakes3(){
+    echo "Schade, das nächste Mal schaffst du es bestimmt"
+}
+mistakes4(){
+    echo "Immer dranbleiben, irgendwann funktioniert es"
+}
+mistakes5(){
+    echo "Kopf hoch, das war nicht das Ende"
+}
