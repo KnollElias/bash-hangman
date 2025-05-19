48e9f31b90e6f98f423f9dce4acb21b027055e81
KnollElias <107244530+KnollElias@users.noreply.github.com>
2025-05-19 13:40:38 +0200

    Update test_startup.sh

--- hangman/tests/test_startup.sh
+++ hangman/tests/test_startup.sh
@@ -5,5 +5,5 @@ source "$(dirname "$0")/../display.sh"
 
 # display_startup should print the welcome + length
 out=$(display_startup "foobar" 2>&1)
-[[ "$out" == *"Willkommen zu Hängmännli!"* ]]
+[[ "$out" == *"Willkommen zu Hangman!"* ]]
 [[ "$out" == *"Das Wort hat 6 Buchstaben."* ]]
