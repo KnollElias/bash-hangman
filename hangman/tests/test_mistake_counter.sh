8df4f550321d5f93e12675a1dd2cc485067d269c
silvanhorn <100518037+silvanhorn@users.noreply.github.com>
2025-05-29 20:55:29 +0200

    added a test

---
+++ hangman/tests/test_mistake_counter.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -uoe pipefail
+
+source "$(dirname "$0")/../main.sh"
+
+test_mistake_countin(){
+
+local guessed_ok=()
+local guessed_bad=()
+local wrongstate=0
+local secret="Apfelsaft"
+
+
+# test 1
+
+mistake_countin "a" "$secret" guessed_ok guessed_bad wrongstate 
+
+echo $wrongstate
+
+[[ $wrongstate == 0 ]] &&  echo succes 
+echo 
+
+
+# test 2 
+echo hier tuts
+
+
+
+
+ 
+set +e
+
+mistake_countin "x" "$secret" guessed_ok guessed_bad wrongstate
+
+echo $wrongstate
+
+[[ $wrongstate == 1 ]] || exit 1
+
+
+
+ mistake_countin "x" "$secret" guessed_ok guessed_bad wrongstate
+
+ echo $wrongstate
+
+ [[ $wrongstate == 1 ]] || exit 1 #should remaim the same because x was already guessed. 
+
+
+
+
+
+}
+
+
+test_mistake_countin
+
+
+
+#mistake_countin "B" "Buchstabe" 
+
+
+
+#echo ${#guessed_bad[@]}
+
+#for ()
