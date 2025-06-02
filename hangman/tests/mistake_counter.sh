e2837acb77b0308501ac0a3c58b0137e4bf42467
silvanhorn <100518037+silvanhorn@users.noreply.github.com>
2025-06-02 14:45:13 +0200

    added the missing tests to the repo

---
+++ hangman/tests/mistake_counter.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -uoe pipefail
+
+source "$(dirname "$0")/../main.sh"
+
+test_mistake_countin() {
+
+  local guessed_ok=()
+  local guessed_bad=()
+  local wrongstate=0
+  local secret="Apfelsaft"
+
+  # test 1
+
+  mistake_countin "a" "$secret" guessed_ok guessed_bad wrongstate
+
+  [[ $wrongstate == 0 ]]
+
+  # test 2
+
+  set +e
+
+  mistake_countin "x" "$secret" ok_guess bad_guess wrongstate
+  #echo $wrongstate
+
+  [[ $wrongstate == 1 ]] || exit 1
+
+  mistake_countin "x" "$secret" ok_guess bad_guess wrongstate
+
+  [[ $wrongstate == 1 ]] || exit 1 #should remaim the same because x was already guessed.
+
+}
+
+test_mistake_countin
