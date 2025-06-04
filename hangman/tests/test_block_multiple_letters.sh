34d610d021be18a7545fb6e40f41580704da4512
silvanhorn <100518037+silvanhorn@users.noreply.github.com>
2025-06-04 17:51:49 +0200

    small changes

--- hangman/tests/test_block_multiple_letters.sh
+++ hangman/tests/test_block_multiple_letters.sh
@@ -12,19 +12,18 @@ wrong_value=0
 
 mistake_treatement ok_test bad_test $ltr wrong_value 
 
-if [[ ${#ok_test[@]} == 0 &&  ${#bad_test[@]} == 1 ]]; then  # testing if the array if the arrays
+if ! [[ ${#ok_test[@]} == 0 &&  ${#bad_test[@]} == 1 ]]; then  # testing if the array if the arrays
 # are not manipulated. 
-echo succes 
-else 
-echo kein succes 
-echo ${bad_test[@]} 
-echo ${ok_test[@]}
+exit 1
+
 fi 
 
 
 # testing if there is the correct user_error in case of trying a letter again. 
 
-[[ $user_error == "schon geraten" ]] && echo succes 
+
+
+[[ $user_error == "schon geraten" ]] && echo succes || exit 1 # more safety
 
 
 
